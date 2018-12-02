82586a4074b7039fe39bb009314a1a67eabe1f27
Luke Artnak <lartnak@gmail.com>
2018-12-01 20:28:54 -0500

    c4faf5a

---
+++ precache-manifest.7f96360c0b90e5b3307e3adf9f492405.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea5de5de675b632c4eb",
+    "url": "/nhl-dashboard/static/js/main.6ea5de5d.chunk.js"
+  },
+  {
+    "revision": "c212b6b0917697bd67cd",
+    "url": "/nhl-dashboard/static/js/1.c212b6b0.chunk.js"
+  },
+  {
+    "revision": "b77ee42b6dbf545d88e1",
+    "url": "/nhl-dashboard/static/js/runtime~main.b77ee42b.js"
+  },
+  {
+    "revision": "2817673dedfd070028d5d3720edbc2a9",
+    "url": "/nhl-dashboard/index.html"
+  }
+];
